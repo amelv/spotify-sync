f421ef0a46f546d0d5c1cb2726c7175ed439aac5
Alex Melvin <alex.melvin99.com>
2020-03-28 14:16:45 -0400

    basic layout of landing and playlist generator page number 1

--- scopify/src/components/GeneratorPage/GeneratorPage.js
+++ scopify/src/components/GeneratorPage/GeneratorPage.js
@@ -10,7 +10,10 @@ import { Button } from 'semantic-ui-react'
 import { Container } from 'semantic-ui-react'
 import { Header } from 'semantic-ui-react'
 import { Divider } from 'semantic-ui-react'
+import { Grid, Image } from 'semantic-ui-react'
+import _ from 'lodash'
 
+import './style.css'
 
   /*const StyledGrid = styled(Grid)`
     margin: auto;
@@ -29,23 +32,71 @@ import { Divider } from 'semantic-ui-react'
     }
 `*/
 
-const GeneratorPage = (props) => {
+
+
+const HoroscopeInput = (props) => {
+  let zodiacIcons = ['♈', '♉', '♊', '♋', '♌', '♍', '♎', '♏', '♐', '♑', '♒', '♓']
+  let zodiacNames = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces']
+  
+  const handleClick = (e) => {
+    let val = e.target.value
+    console.log(val)
+  }
+
+  let grid = _.times(3, (row) => (
+   <Grid.Row style={{display: 'flex', alignItems: 'center', textAlign: 'center', flexWrap: "nowrap"}} key={row}>
+    {_.times(4, (col) => (
+      <Grid.Column key={4*row + col} >
+        <Button onClick={handleClick} value={zodiacNames[4*row + col]} style={{color: "transparent"}} >
+        <Header as='h1' style={{pointerEvents: 'none'}} >{zodiacIcons[4*row + col]}</Header>
+        <Header as='h3' style={{pointerEvents: 'none' }}>{zodiacNames[4*row + col]}</Header>
+      </Button>
+      </Grid.Column>
+    ))}
+    </Grid.Row> 
+  ))
   
   return (
-    <Container>
-      <Header as='h1'>Scopify</Header>
-      <Divider />
-      <Header as='h2'> Horoscoped Playlists </Header>
-      <p>Test test test test test test</p>
-      <Divider />
-      <Button content='Log in to Spotify' />
+    <Grid columns={4} divided>
+      {grid}
+    </Grid>
+  )
+}
 
+const MoodInput = (props) => {
 
-    </Container>
+}
+
+const EnergyInput = (props) => {
+
+}
+
+const NameInput = (props) => {
+
+}
+
+const GeneratorPage = (props) => {
+  let num = props.match.params.num
+  
+  let inputs = [HoroscopeInput, MoodInput, EnergyInput, NameInput]
+  let InputComponent = inputs[num-1]
+
+  console.log(num)
+  return (
+   <Container style={{display: "flex", 
+      background: "rgb(127,24,161)", background: "linear-gradient(90deg, rgba(127,24,161,1) 70%, rgba(20,200,20,1) 100%)", 
+      flexDirection: "column", 
+      alignItems: "center", 
+      justifyContent: "center", 
+     height: "100vh"}} >
+      <InputComponent /> 
+      <Divider />
+
+    </Container> 
   )
 }
 
 GeneratorPage.propTypes = {
 }
 
-export default withRouter(GeneratorPage)
+export default GeneratorPage

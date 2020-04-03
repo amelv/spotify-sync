fa2aa671197d72d5fc1b85e97dd09e580e2d5f90
Alex Melvin <alex.melvin99.com>
2020-04-03 09:23:18 -0400

    removed useless files

--- scopify/src/components/AtlasContext.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-// Possible contexts are:
-// - standalone (default, for appearing in the gallery)
-// - entry (for appearing on an entry page)
-// - card (for appearing in an entry card)
-
-export const AtlasContext = React.createContext("standalone")

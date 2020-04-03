923db17c21348e940c56ad5df0e96a17d97c393d
Alex Melvin <alex.melvin99.com>
2020-04-02 22:33:46 -0400

    added scopes to spotify login authorizaiton

--- scopify/src/components/LandingPage/LandingPage.js
+++ scopify/src/components/LandingPage/LandingPage.js
@@ -1,8 +1,7 @@
-import React, { useContext } from "react"
+import React, { useState, useEffect, useContext } from "react"
 import PropTypes from "prop-types"
 import {Link} from "react-router-dom"
 import Particles from "react-particles-js"
-import styled from "styled-components"
 import { CSSTransition } from "react-transition-group";
 import { withRouter } from "react-router";
 
@@ -13,6 +12,8 @@ import { Divider } from 'semantic-ui-react'
 import SpotifyLogin from 'react-spotify-login';
 import { clientId, redirectUri } from './settings';
 import {AuthContext} from '../AuthContext'
+
+import '../GlobalStyle.css'
 
 const onSuccess = response => console.log(response);
 const onFailure = response => console.error(response);
@@ -36,15 +37,21 @@ const onFailure = response => console.error(response);
 
 const LandingPage = (props) => {
   const { loggedIn, accessToken, setLoggedIn, setToken } = useContext(AuthContext)
+  const [isLoaded, setLoaded] = useState(false)
+
+  useEffect(() => {
+    setLoaded(true)
+  })
 
   return (
+    <CSSTransition classNames='fade' timeout={{enter: 500, exit: 300}} > 
     <Container style={{display: "flex", 
       background: "rgb(127,24,161)",
       flexDirection: "column", 
       alignItems: "center", 
       justifyContent: "center", 
       height: "100vh",
-      }} >
+    }} >
       <Header 
         as='h1' 
         style={{fontSize: "10rem", margin: "1rem"}}>𝕾𝖈𝖔𝖕𝖎𝖋𝖞</Header>
@@ -53,6 +60,7 @@ const LandingPage = (props) => {
           <Button as={SpotifyLogin} 
           clientId={clientId}
           redirectUri={redirectUri}
+          scope={'playlist-modify-public,playlist-modify-private,user-top-read'}
           onSuccess={(response) => {
             setToken(response.access_token)
             setLoggedIn(true)
@@ -63,7 +71,8 @@ const LandingPage = (props) => {
           content='Log In to Spotify'/>
         )} 
         <Button 
-          as={ Link }
+        as={ Link }
+        onClick={() => setLoaded(false)}
           to='/step1' 
           size='massive'
           style={{margin: "1rem"}} 
@@ -71,6 +80,7 @@ const LandingPage = (props) => {
       <Divider />
 
     </Container>
+  </CSSTransition>
   )
 }
 

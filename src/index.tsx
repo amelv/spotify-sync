fd399b330d64c51956ad1947180023521c5b97dd
Alex Melvin <amelv@protonmail.com>
2022-04-18 19:54:12 -0400

    Reset app with new create react app instance with typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
-import React, {Suspense, lazy} from "react"
-import ReactDOM from "react-dom"
-import {BrowserRouter} from "react-router-dom"
-//import Loading from "./components/Loading/Loading"
-import "semantic-ui-css/semantic.min.css"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import {Scopify} from "./components/Scopify"
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-ReactDOM.render((
-    <BrowserRouter>
-        <React.Fragment>
-            <Suspense>
-				<Scopify />
-            </Suspense>
-        </React.Fragment>
-    </BrowserRouter>
-), document.getElementById("root"));
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();

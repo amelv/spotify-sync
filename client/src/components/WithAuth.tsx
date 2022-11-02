e3b683ddde17e5b3e9b0048eaea4e4d130ca9c28
amelv <amelv@protonmail.com>
2022-11-02 17:22:06 -0400

    only nav to login page when no acces AND refresh

--- client/src/components/WithAuth.tsx
+++ client/src/components/WithAuth.tsx
@@ -5,6 +5,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { useTokenRefresh } from "src/hooks";
 
 import { useHydration, useStore } from "src/store";
+import shallow from "zustand/shallow";
 
 const LoadingAccess = () => (
   <Container sx={{ display: "flex", justifyContent: "center" }}>
@@ -15,18 +16,18 @@ const LoadingAccess = () => (
 );
 
 export const withAuth = (Component: FunctionComponent) => (props: any) => {
-  const { access } = useStore((state) => state.tokens);
+  const { access, refresh } = useStore((state) => state.tokens, shallow);
   const isHydrated = useHydration();
   const location = useLocation();
   const navigate = useNavigate();
 
+  useTokenRefresh();
+
   useEffect(() => {
-    if (isHydrated && !access) {
+    if (isHydrated && !access && !refresh) {
       navigate("/login", { replace: true, state: { from: location } });
     }
   }, [access, navigate, location, isHydrated]);
-
-  useTokenRefresh();
 
   return isHydrated ? <Component {...props} /> : <LoadingAccess />;
 };
